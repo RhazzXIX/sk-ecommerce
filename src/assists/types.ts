a7c6eb839eb024bfe1fcb11cded4e580ef8b332d
RhazzXIX <jm21tonio@gmail.com>
2023-06-02 23:17:54 +0800

    Create separate types file

---
+++ src/assists/types.ts
@@ -0,0 +1,14 @@
+type product = {
+  brand: string;
+  category: string;
+  description: string;
+  id: number;
+  images: [string];
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
+  discountPercentage: number;
+  qty?: number;
+};
