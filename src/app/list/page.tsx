db70be606316dcc4dccfce9d7c3254b780296e28
RhazzXIX <jm21tonio@gmail.com>
2023-06-03 01:26:43 +0800

    Add logic to show/hide cart

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import ProductSection from "@/components/ProductSection";
-import { useState } from "react";
+import { SyntheticEvent, useState, MouseEventHandler } from "react";
+import Cart from "@/components/Cart";
+import Image from "next/image";
+import cartImg from "../../../public/cart.svg";
 
 export default function VList() {
   const [cartItems, setCartItems] = useState<product[]>([]);
+  const [showCart, setShowCart] = useState(false);
 
   const addToCart = (product: product) => {
     const [alreadyInCart] = cartItems.filter((item) => item.id === product.id);
@@ -11,20 +15,43 @@ export default function VList() {
     setCartItems(cartItems.concat(product));
   };
 
-  const removeFromCart = (e: React.ChangeEvent<HTMLButtonElement>) => {
-    const productId = Number(e.target.dataset.id);
-    setCartItems(
-      cartItems.filter((item) => {
-        return item.id !== productId;
-      })
-    );
+  const removeFromCart = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    if (e.target instanceof HTMLElement) {
+      const productId = Number(e.target.dataset.id);
+      setCartItems(
+        cartItems.filter((item) => {
+          return item.id !== productId;
+        })
+      );
+    }
+  };
+
+  const toggleCart = () => {
+    if (showCart) setShowCart(false);
+    setShowCart(true);
+  };
+
+  const closeCart = (e: SyntheticEvent) => {
+    const cart = document.querySelector("section#cart-page");
+    if (!cart) return;
+    if (cart?.contains(e.target as HTMLElement)) return;
+    setShowCart(false);
   };
 
   return (
-    <main>
-      <section>
-        <h1>Categories</h1>
-      </section>
+    <main onClick={closeCart}>
+      <header>
+        <h2>Products:</h2>
+        <button id="cart" aria-label="Cart" onClick={toggleCart}>
+          <Image src={cartImg} alt="Cart" />
+          {cartItems.length !== 0 && <p>{cartItems.length}</p>}
+        </button>
+        {showCart && (
+          <Cart products={cartItems} deleteProduct={removeFromCart} />
+        )}
+      </header>
       <ProductSection addToCart={addToCart} />
     </main>
   );
--- src/tests/VList.test.js
+++ src/tests/VList.test.js
@@ -1,7 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import VList from "../app/list/page";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
 global.fetch = jest.fn();
+const user = userEvent.setup();
 
 const newItems = {
   products: [
@@ -51,5 +54,29 @@ describe("List component", () => {
     const { container } = render(<VList />);
     await screen.findByText("iPhone 9");
     expect(container).toMatchSnapshot();
+  });
+  it(`Let's users see what in the cart`, async () => {
+    render(<VList />);
+    const cartBtn = screen.getByRole("button", { name: "Cart" });
+
+    expect(
+      screen.queryByRole("heading", { name: "Cart" })
+    ).not.toBeInTheDocument();
+
+    await user.click(cartBtn);
+
+    expect(screen.getByRole("heading", { name: "Cart" })).toBeInTheDocument();
+  });
+
+  it(`Let's the user close the cart by clicking anywhere on the Shop page`, async () => {
+    render(<VList />);
+    const cartBtn = screen.getByRole("button", { name: "Cart" });
+    const shopPage = screen.getByRole("main");
+    await user.click(cartBtn);
+    expect(screen.getByRole("heading", { name: "Cart" })).toBeInTheDocument();
+    await user.click(shopPage);
+    expect(
+      screen.queryByRole("heading", { name: "Cart" })
+    ).not.toBeInTheDocument();
   });
 });
